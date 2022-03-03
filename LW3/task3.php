63fcf01eef27cb86d736b65fe33f232e910ca163
Deniska10K <sorokin20072003@yandex.ru>
2022-03-03 08:01:46 +0300

    upload

--- LW3/task3.php
+++ LW3/task3.php
@@ -10,26 +10,26 @@ $duplicate_symbols = 0;
 
 foreach (count_chars($password) as $symbol_count)
 {
-    $symbol_count > 1 ? $duplicate_symbols += $symbol_count : null; // duplicate_symbols
+    $symbol_count > 1 ? $duplicate_symbols += $symbol_count : null; // duplicate symbols
 }
 
 
 foreach (str_split($password , 1 ) as $symbol)
 {
-    ctype_digit($symbol) ? $digits_count++ : null; // digits_count
-    ctype_upper($symbol) ? $uppers_count++ : null; // uppers_count
-    ctype_lower($symbol) ? $lowers_count++ : null; // lowers_count
+    ctype_digit($symbol) ? $digits_count++ : null; // digits count
+    ctype_upper($symbol) ? $uppers_count++ : null; // uppers count
+    ctype_lower($symbol) ? $lowers_count++ : null; // lowers count
 }
 
 
 // Calculate
 $strength = 0; // basic
 $strength += $password_len * 4; // password_len
-$strength += ($password_len - $uppers_count) * 2; // uppers_count
-$strength += ($password_len - $lowers_count) * 2; // lowers_count
+$strength += ($password_len - $uppers_count) * 2; // uppers count
+$strength += ($password_len - $lowers_count) * 2; // lowers count
 ctype_alpha($password) ? $strength -= $password_len : null; // only letters
 ctype_digit($password) ? $strength -= $password_len : null; // only digits
-$strength -= $duplicate_symbols; // duplicate_symbols
+$strength -= $duplicate_symbols; // duplicate symbols
 
 
 // Output
